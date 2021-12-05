7a741054eb45166e5f0418192d6a071297b69097
Martin Lindgren <martin.l@totalproduce.se>
2021-12-05 14:56:08 +0100

    Puzzle 1-4

---
+++ 2021/AOC2021/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOC2021.Puzzles;
+
+var type = typeof(IPuzzle);
+var puzzles = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+    .Select(Activator.CreateInstance)
+    .OfType<IPuzzle>();
+
+Console.WriteLine("Advent Of Code 2021");
+Console.WriteLine(new string('-', 30));
+foreach (var puzzle in puzzles)
+{
+    Console.WriteLine(puzzle);
+}
+
+Console.ReadLine();
