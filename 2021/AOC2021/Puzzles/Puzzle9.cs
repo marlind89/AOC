1c9677674fee481c4242780cf6973b34aaf1a5eb
Martin Lindgren <martin.l@totalproduce.se>
2021-12-11 11:35:51 +0100

    Day 11

--- 2021/AOC2021/Puzzles/Puzzle9.cs
+++ 2021/AOC2021/Puzzles/Puzzle9.cs
@@ -1,25 +1,16 @@
-﻿namespace AOC2021.Puzzles
+﻿using AOC2021.Helpers;
+
+namespace AOC2021.Puzzles
 {
     internal class Puzzle9 : Puzzle<int>
     {
-        private static readonly (int x, int y)[] NeighborOffsets = new[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
-
-        private int _xLength;
-        private int _yLength;
         private int[,] _map = default!;
 
         protected override void Solve(string[] lines)
         {
-            _xLength = lines[0].Length;
-            _yLength = lines.Length;
-            _map = new int[_xLength, _yLength];
+            _map = Grid.CreateGrid(lines);
 
-            foreach (var (x, y) in Iterate(_map))
-            {
-                _map[x, y] = lines[y][x] - '0';
-            }
-
-            var lowPoints = Iterate(_map).Where(IsLowPoint).ToArray();
+            var lowPoints = Grid.Iterate(_map).Where(IsLowPoint).ToArray();
 
             One = lowPoints.Sum(coord => 1 + _map[coord.x, coord.y]);
             Two = lowPoints
@@ -29,8 +20,8 @@
                 .Aggregate(1, (a,b) => a * b);
         }
 
-        private bool IsLowPoint((int x, int y) coord) => NeighborOffsets
-            .Select(offset => GetDepth(coord.x + offset.x, coord.y + offset.y, int.MaxValue))
+        private bool IsLowPoint((int x, int y) coord) => Grid.NeighborOffsets
+            .Select(offset => GetDepth((coord.x + offset.x, coord.y + offset.y), int.MaxValue))
             .All(n => n > _map[coord.x, coord.y]);
 
         private int GetBasinSize((int x, int y) coord, ISet<(int x, int y)> visitedNodes)
@@ -46,29 +37,17 @@
                 return 0;
             }
 
-            return 1 + NeighborOffsets
+            return 1 + Grid.NeighborOffsets
                 .Select(offset => new
                 {
                     coord = (coord.x + offset.x, coord.y + offset.y),
-                    depth = GetDepth(coord.x + offset.x, coord.y + offset.y, -1)
+                    depth = GetDepth((coord.x + offset.x, coord.y + offset.y), -1)
                 })
                 .Where(x => x.depth > depth)
                 .Sum(x => GetBasinSize(x.coord, visitedNodes));
         }
 
-        private int GetDepth(int x, int y, int defaultValue) => IsOutOfRange(x, y) ? defaultValue : _map[x, y];
-
-        private bool IsOutOfRange(int x, int y) => x < 0 || x >= _xLength || y < 0 || y >= _yLength;
-
-        private static IEnumerable<(int x, int y)> Iterate(int[,] arr)
-        {
-            for (int x = 0; x < arr.GetLength(0); x++)
-            {
-                for (int y = 0; y < arr.GetLength(1); y++)
-                {
-                    yield return (x, y);
-                }
-            }
-        }
+        private int GetDepth((int x, int y) coord, int defaultValue) =>
+            Grid.IsOutOfRange(_map, coord) ? defaultValue : _map[coord.x, coord.y];
     }
 }
