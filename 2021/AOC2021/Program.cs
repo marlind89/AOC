78cd5a5a13dd7a98d51adce40afd05036182fc55
Martin Lindgren <martin.l@totalproduce.se>
2021-12-15 13:32:04 +0100

    Add execution time to console output

--- 2021/AOC2021/Program.cs
+++ 2021/AOC2021/Program.cs
@@ -1,19 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AOC2021.Puzzles;
+using System.Diagnostics;
 
 var type = typeof(IPuzzle);
 var puzzles = AppDomain.CurrentDomain.GetAssemblies()
     .SelectMany(s => s.GetTypes())
     .Where(p => type.IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
     .Select(Activator.CreateInstance)
-    .OfType<IPuzzle>();
+    .OfType<IPuzzle>()
+    .OrderBy(x => x.PuzzleNumber)
+    .ToList();
 
+var sw = new Stopwatch();
 Console.WriteLine("Advent Of Code 2021");
 Console.WriteLine(new string('-', 30));
-foreach (var puzzle in puzzles.OrderBy(x => x.PuzzleNumber))
+sw.Start();
+foreach (var puzzle in puzzles)
 {
     puzzle.Solve();
+}
+sw.Stop();
+
+foreach (var puzzle in puzzles)
+{
     Console.WriteLine(puzzle);
 }
+
+Console.WriteLine(new string('-', 30));
+Console.WriteLine($"Total time (ms): {sw.ElapsedMilliseconds}");
 
 Console.ReadLine();
