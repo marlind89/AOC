6e02617eda94faa67ce6f9b21282c0a6669dc887
Martin Lindgren <martin.l@totalproduce.se>
2022-12-01 22:24:11 +0100

    2022 Day 1

---
+++ 2022/Puzzles/Puzzle1.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace AOC2022.Puzzles;
+
+internal class Puzzle1 : Puzzle<int>
+{
+    protected override void Solve(string[] lines)
+    {
+        var elves = lines.Aggregate(new List<int>() { 0 }, (cals, line) =>
+        {
+            if (string.IsNullOrWhiteSpace(line)) cals.Add(0);
+            else cals[^1] += int.Parse(line);
+            return cals;
+        }).OrderByDescending(x => x);
+
+        One = elves.First();
+        Two = elves.Take(3).Sum();
+    }
+}
