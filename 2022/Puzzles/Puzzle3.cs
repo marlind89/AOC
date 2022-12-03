1e72e2b71d68b43a17df464f11bf15026440e816
Martin Lindgren <martin.l@totalproduce.se>
2022-12-03 18:03:45 +0100

    2022 Day 3

---
+++ 2022/Puzzles/Puzzle3.cs
@@ -0,0 +1,20 @@
+﻿namespace AOC2022.Puzzles;
+
+internal class Puzzle3 : Puzzle<int>
+{
+    protected override void Solve(string[] lines)
+    {
+        One = lines
+            .Sum(line => line.Substring(0, line.Length / 2)
+                .Intersect(line.Substring(line.Length / 2))
+                .Select(GetPriority)
+                .Single());
+        Two = lines
+            .Chunk(3)
+            .Sum(group => group
+                .Skip(1)
+                .Aggregate(group.First(), (a, b) => string.Concat(a.Intersect(b)), x => GetPriority(x.Single())));
+    }
+
+    private static int GetPriority(char type) => type - (char.IsUpper(type) ? 38 : 96);
+}
