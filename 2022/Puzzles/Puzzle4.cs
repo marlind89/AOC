f0c9cfd314f6be8fdca561ed1662d6ee60d0f2b6
Martin Lindgren <martin.l@totalproduce.se>
2022-12-04 11:02:54 +0100

    2022 Day 4

---
+++ 2022/Puzzles/Puzzle4.cs
@@ -0,0 +1,18 @@
+﻿namespace AOC2022.Puzzles;
+
+internal class Puzzle4 : Puzzle<int>
+{
+    protected override void Solve(string[] lines)
+    {
+        var overlaps = lines
+            .Select(x => x.Split(',')
+                .Select(range => range.Split('-').Select(int.Parse).ToList())
+                .ToList())
+            .Where(ranges => ranges[0][0] <= ranges[1][1] && ranges[1][0] <= ranges[0][1])
+            .ToList();
+
+        One = overlaps.Count(ranges => (ranges[0][0] <= ranges[1][0] && ranges[0][1] >= ranges[1][1]) ||
+            (ranges[1][0] <= ranges[0][0] && ranges[1][1] >= ranges[0][1]));
+        Two = overlaps.Count;
+    }
+}
