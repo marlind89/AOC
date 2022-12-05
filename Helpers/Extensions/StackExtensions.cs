e404dd0870f3462471a9bcac250a0152eb6bb98c
Martin Lindgren <martin.l@totalproduce.se>
2022-12-05 21:31:12 +0100

    2022 Day 5

---
+++ Helpers/Extensions/StackExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AOC.Helpers.Extensions;
+
+public static class StackExtensions
+{
+    public static void PushRange<T>(this Stack<T> stack, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            stack.Push(item); 
+        }
+    }
+}
