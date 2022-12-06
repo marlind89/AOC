a375dd8b130546c83e309924ee2b123f89c7231c
Martin Lindgren <martin.l@totalproduce.se>
2022-12-06 16:02:28 +0100

    2022 Day 6

---
+++ Helpers/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AOC.Helpers.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<IReadOnlyCollection<T>> SlidingWindow<T>(
+        this IEnumerable<T> source, int amount)
+    {
+        var enumerators = Enumerable.Range(0, amount)
+            .Select(x => source.Skip(x).GetEnumerator())
+            .ToList();
+
+        while (enumerators.All(x => x.MoveNext()))
+        {
+            yield return enumerators.Select(x => x.Current).ToList();
+        }
+    }
+}
