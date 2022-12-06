529f4784bebf2a847427851be9fa0569c53acf24
Martin Lindgren <martin.l@totalproduce.se>
2022-12-06 16:02:28 +0100

    2022 Day 6

---
+++ 2022/Puzzles/Puzzle6.cs
@@ -0,0 +1,17 @@
+﻿using AOC.Helpers.Extensions;
+
+namespace AOC2022.Puzzles;
+
+internal partial class Puzzle6 : Puzzle<int>
+{
+    protected override void Solve(string[] lines)
+    {
+        One = FindMarker(lines[0], 4);
+        Two = FindMarker(lines[0], 14);
+    }
+
+    private static int FindMarker(string message, int windowLength) => message
+        .SlidingWindow(windowLength)
+        .Select((windowStr, idx) => new HashSet<char>(windowStr).Count == windowLength ? (idx + windowLength) : -1)
+        .First(x => x > 0);
+}
