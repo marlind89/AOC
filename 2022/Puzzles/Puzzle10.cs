f98942b234a871673bd6a0a0018b6d0712155be5
Martin Lindgren <martin.l@totalproduce.se>
2022-12-10 18:32:08 +0100

    2022 Day 10

---
+++ 2022/Puzzles/Puzzle10.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace AOC2022.Puzzles;
+
+internal class Puzzle10 : Puzzle<int, string>
+{
+    private const int CrtPixels = 40;
+
+    protected override void Solve(string[] lines)
+    {
+        var (x, totalCycles, signalStrengths, drawer) = (1, 0, new Dictionary<int, int>(), new StringBuilder());
+        foreach (var line in lines)
+        {
+            var (cycles, addx) = ParseInstruction(line);
+            foreach (var currCycle in Enumerable.Range(totalCycles + 1, cycles))
+            {
+                signalStrengths[currCycle] = x * currCycle;
+                var crtIndex = (currCycle - 1) % CrtPixels;
+                var pixel = crtIndex >= x - 1 && crtIndex <= x + 1 ? '#' : '.';
+
+                drawer.Append(pixel);
+                if (currCycle / CrtPixels > ((currCycle - 1) / CrtPixels)) 
+                {
+                    drawer.AppendLine();
+                }
+            }
+
+            totalCycles += cycles;
+            x += addx;
+        }
+
+        One = Enumerable.Range(0, 6).Select(x => signalStrengths[20 + 40 * x]).Sum();
+        Two = Environment.NewLine + Environment.NewLine + drawer.ToString();
+    }
+
+    private static (int cycles, int addx) ParseInstruction(string instruction) => instruction switch
+    {
+        "noop" => (1, 0),
+        _ => (2, int.Parse(instruction.Split(' ')[1]))
+    };
+}
