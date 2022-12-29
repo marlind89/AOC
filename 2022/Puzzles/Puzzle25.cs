a89fa7d54d3288428a41422ee6f1726d817e39dd
Martin Lindgren <martin.l@totalproduce.se>
2022-12-29 10:39:11 +0100

    2022 Day 25

---
+++ 2022/Puzzles/Puzzle25.cs
@@ -0,0 +1,46 @@
+﻿namespace AOC2022.Puzzles;
+
+internal class Puzzle25 : Puzzle<string>
+{
+    protected override void Solve(string[] lines)
+    {
+        One = ToSnafu(lines
+            .Select(line => Enumerable.Range(1, line.Length)
+                .Sum(c => ToDecimal(line[^c]) * (long)Math.Pow(5, c - 1)))
+            .Sum());
+    }
+
+    private static int ToDecimal(char snafu) => snafu switch
+    {
+        '=' => -2,
+        '-' => -1,
+        _ => snafu - '0',
+    };
+
+    private static string ToSnafu(long value)
+    {
+        var (carry, rem, values) = (0, 0, new List<int>());
+
+        while (value > 0)
+        {
+            rem = (int) (value % 5) + (rem < 0 ? 1 : 0);
+            if (rem > 2)
+            {
+                rem -= 5;
+            }
+            values.Add(rem);
+            value /= 5;
+        }
+
+        return values
+            .Reverse<int>()
+            .Aggregate("", (acc, cur) =>
+                acc + cur switch
+                {
+                    -2 => "=",
+                    -1 => "-",
+                    _ => cur
+                });
+        
+    }
+}
